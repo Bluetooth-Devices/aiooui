087b021de98831abb39d45537c20fcf134651c0a
J. Nick Koston <nick@koston.org>
2025-01-18 11:32:16 -1000

    fix: improve retry when oui data is not available during build (#33)

--- build_oui.py
+++ build_oui.py
@@ -2,8 +2,10 @@
 
 from __future__ import annotations
 
+import asyncio
 import os
 import pathlib
+import time
 from typing import Any
 
 import setuptools
@@ -22,12 +24,20 @@ def build(setup_kwargs: dict[str, Any]) -> None:
             "bdist_wheel": {"plat_name": "any"},
         },
     )
+    if os.environ.get("AIOOUI_SKIP_REGENERATE"):
+        print("Skipping OUI data regeneration.")
+        return
 
-    for _ in range(3):
+    for attempt in range(6):
         try:
-            _regenerate_ouis()
+            if attempt % 2 == 0:
+                asyncio.run(_regenerate_ouis_aiohttp())
+            else:
+                _regenerate_ouis_requests()
+            print(f"Regenerated OUI data on attempt {attempt + 1}.")
             return
         except Exception as e:
+            time.sleep(5)
             print(f"Failed to regenerate OUI data: {e}")
 
     if os.environ.get("AIOOUI_REQUIRE_REGENERATE"):
@@ -36,12 +46,24 @@ def build(setup_kwargs: dict[str, Any]) -> None:
     print("Using existing data.")
 
 
-def _regenerate_ouis() -> None:
+def _regenerate_ouis_requests() -> None:
     import requests  # type: ignore
 
     resp = requests.get("https://standards-oui.ieee.org/oui.txt", timeout=20)
     resp.raise_for_status()
-    oui_bytes = resp.content
+    _update_from_oui_content(resp.content)
+
+
+async def _regenerate_ouis_aiohttp() -> None:
+    import aiohttp
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get("https://standards-oui.ieee.org/oui.txt") as resp:
+            resp.raise_for_status()
+            _update_from_oui_content(await resp.read())
+
+
+def _update_from_oui_content(oui_bytes: bytes) -> None:
     oui_to_vendor = {}
     for line in oui_bytes.splitlines():
         if b"(base 16)" in line:
@@ -50,7 +72,7 @@ def _regenerate_ouis() -> None:
     file = pathlib.Path(__file__)
     target_file = file.parent.joinpath("src").joinpath("aiooui").joinpath("oui.data")
     with open(target_file, "wb") as f:
-        f.write(b"\n".join(b"=".join((o, v)) for o, v in oui_to_vendor.items()))
+        f.write(b"\n".join(b"=".join((o, v)) for o, v in sorted(oui_to_vendor.items())))
 
 
 if __name__ == "__main__":
