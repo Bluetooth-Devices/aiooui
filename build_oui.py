daaf6d5914db76dbb62791b99969c673bf605da1
J. Nick Koston <nick@koston.org>
2025-01-18 11:18:43 -1000

    fix: add another way to generate date

--- build_oui.py
+++ build_oui.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+import asyncio
 import os
 import pathlib
 import time
@@ -24,9 +25,13 @@ def build(setup_kwargs: dict[str, Any]) -> None:
         },
     )
 
-    for _ in range(3):
+    for attempt in range(6):
         try:
-            _regenerate_ouis()
+            if attempt % 2 == 0:
+                asyncio.run(_regenerate_ouis_aiohttp())
+            else:
+                _regenerate_ouis_requests()
+            print(f"Regenerated OUI data on attempt {attempt + 1}.")
             return
         except Exception as e:
             time.sleep(5)
@@ -38,12 +43,24 @@ def build(setup_kwargs: dict[str, Any]) -> None:
     print("Using existing data.")
 
 
-def _regenerate_ouis() -> None:
+def _regenerate_ouis_requests() -> None:
     import requests  # type: ignore
 
     resp = requests.get("https://standards-oui.ieee.org/oui.txt", timeout=20)
     resp.raise_for_status()
-    oui_bytes = resp.content
+    _update_from_oui_content(resp.content)
+
+
+async def _regenerate_ouis_aiohttp() -> None:
+    import aiohttp
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get("https://standards-oui.ieee.org/oui.txt") as resp:
+            resp.raise_for_status()
+            _update_from_oui_content(await resp.read())
+
+
+def _update_from_oui_content(oui_bytes: bytes) -> None:
     oui_to_vendor = {}
     for line in oui_bytes.splitlines():
         if b"(base 16)" in line:
